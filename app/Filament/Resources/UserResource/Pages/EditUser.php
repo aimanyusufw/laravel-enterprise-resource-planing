5abf933da85312c19522fb55d9858a1f3d5e92e8
aimanyusufw <aimanyusufdev@gmail.com>
2025-07-04 08:53:28 +0700

    feat : add role activity log

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -3,12 +3,31 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\Role;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function afterSave(): void
+    {
+        $user = $this->record;
+        $roles = $user->getRoleNames()->toArray();
+
+        if (!empty($roles)) {
+            activity()
+                ->event("attach roles")
+                ->performedOn($user)
+                ->causedBy(auth()->user())
+                ->withProperties([
+                    'user' => $user->username,
+                    'roles' => $roles
+                ])
+                ->log('User roles updated');
+        }
+    }
 
     protected function getHeaderActions(): array
     {
